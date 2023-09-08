e274dca97a3caca87488bcf9647deccc08ca1618
Tolga1452 <tolgchu-studios@outlook.com>
2023-09-09 00:58:18 +0300

    rarerer

--- src/bot.js
+++ src/bot.js
@@ -150,7 +150,7 @@ client.on('interactionCreate', async interaction => {
     
                     let possibility = randomNumber(0, 100);
     
-                    if (possibility > 10) return;
+                    if (possibility < 90) return;
                 } else if (!guild?.aiChannel?.status || message.channel.id !== guild?.aiChannel?.channel) return;
             };
     
