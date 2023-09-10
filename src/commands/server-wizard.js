f1be90bd7ac00cd2ea0c6ad072f04c634177d17c
Tolga1452 <tolgchu-studios@outlook.com>
2023-09-10 14:00:47 +0300

    i hope so

--- src/commands/server-wizard.js
+++ src/commands/server-wizard.js
@@ -435,7 +435,7 @@ module.exports = {
                 },
                 {
                     role: 'system',
-                    content: 'Great! Now you are ready to setup channels for a server. Do not forget, YOU WILL ONLY RESPOND WITH ARRAY OF CHANNELS. NOT WITH ANYTHING ELSE. YOU HAVE TO USE ID FOR CUSTOM ROLE ICONS, NOT NAMES. And you will use your creativity to setup roles for a server, especially for role names and colors. You do not have to create a role for each available icon.'
+                    content: 'Great! Now you are ready to setup channels for a server. Do not forget, YOU WILL ONLY RESPOND WITH ARRAY OF CHANNELS. NOT WITH ANYTHING ELSE. And you will use your creativity to setup roles for a server, especially for role names and colors. You do not have to create a role for each available icon.\n\nYOU WILL NOT USE ICON NAME FOR ICON ID. YOU WILL ONLY USE ICON ID OR UNICODE EMOJI.'
                 },
                 {
                     role: 'user',
